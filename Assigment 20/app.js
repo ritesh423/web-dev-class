e0d1b8ee34684aa28174eac3a7fec2ed61da4fa6
Ritesh-web-dev <74403818+Ritesh-web-dev@users.noreply.github.com>
2020-11-24 21:20:11 +0530

    Assigment 20

---
+++ Assigment 20/app.js
@@ -0,0 +1,7 @@
+console.log("Assigment 20 by Ritesh");
+let startingName = prompt("Enter your Firstname!");
+let endingName = prompt("Enter your Lastname1!");
+let howMuchOld = prompt("Enter your Age!");
+let grader = prompt("In which grade you are?")
+console.log("Your name is " + startingName + " " + endingName);
+console.log("Your Age is " + howMuchOld + " and you are in " + grader + " grade");
