189929b5e577dea3b49807b0e48186c1eb2a4964
Ritesh-web-dev <74403818+Ritesh-web-dev@users.noreply.github.com>
2020-11-25 14:38:59 +0530

    assigment 21
    assigment 21

---
+++ Assigmnet 21/app.js
@@ -0,0 +1,13 @@
+let ageOfThePerson = prompt("Enter You Age");
+alert("Your age is roughly around " + ageOfThePerson * 365 + " days");
+
+let rectangleAreaL = prompt("Enter the length of rectangle");
+let rectangleAreaB = prompt("Enter the breadth of rectangle");
+alert("Area of rectangle is " + rectangleAreaL * rectangleAreaB + " According to the values given by you");
+
+let triangleHeight = prompt("Enter the height of triangle")
+let triangleBase = prompt("Enter the Base of triangle")
+alert("Area of triangle is " + triangleHeight * triangleBase / 2 + " According to the values given by you");
+
+let circleArea = prompt("Enter the radius for circle");
+alert("Area of circle is " + 22/7 * circleArea * circleArea + " According to the values given by you")
